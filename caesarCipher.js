e38fbc457d1a095f24bc073b7566a3b006bd0ae9
Max Dendy <80255339+MaxKamenskii@users.noreply.github.com>
2026-01-08 22:08:19 +0500

    Implement wrapping

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,5 +1,21 @@
+const isLowerCase = (str) => str === str.toLowerCase();
+const isUpperCase = (str) => str === str.toUpperCase();
+
 function convertOneCharacter(char, value) {
+  // Turn char into code + value
+  // check if lower case
+  if (isLowerCase(char)) {
+    if (char.charCodeAt() + value > 122) {
+      return String.fromCharCode(char.charCodeAt() + value - 122 + 96);
+    }
+  }
+  if (isUpperCase(char)) {
+    if (char.charCodeAt() + value > 90 && char.charCodeAt() + value < 97) {
+      return String.fromCharCode(char.charCodeat() + value - 90 + 64);
+    }
+  }
   char = char.charCodeAt() + value;
+  // return char
   return String.fromCharCode(char);
 }
 
@@ -9,6 +25,6 @@ function caesarCipher(string, value) {
   return string.join("");
 }
 
-console.log(caesarCipher("aaa", 3));
+console.log(caesarCipher("AaA", 3));
 
 module.exports = caesarCipher;
